1e25776a1734bbae31e6fb752038aa47a009142b
Mitchell <mitchelltogden@gmail.com>
2020-07-18 11:26:21 -0400

    Add powershell scripts for win7 to win10 HIDscripts

---
+++ fresh-win7-to-win10.ps1
@@ -0,0 +1,13 @@
+$dir = "c:\temp"
+
+mkdir $dir
+
+$webClient = New-Object System.Net.WebClient
+
+$url = "https://go.microsoft.com/fwlink/?LinkID=799445"
+
+$file = "$($dir)\Win10Upgrade.exe"
+
+$webClient.DownloadFile($url,$file)
+
+Start-Process -FilePath $file -ArgumentList "/quietinstall /skipeula /auto clean /copylogs $dir" -verb runas
